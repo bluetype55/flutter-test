f8d390562b347f9cf4eff1ac9a303c23f7e1b7b2
bluetype55 <118740628+bluetype55@users.noreply.github.com>
2023-08-28 21:30:36 +0900

    weather app: fetching data & json parsing

--- advanced/weather_app/lib/screens/loading.dart
+++ advanced/weather_app/lib/screens/loading.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -9,23 +11,44 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-  void getLocation() async {
-    LocationPermission permission = await Geolocator.requestPermission();
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high);
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+    fetchData();
+  }
 
-    print(position);
+  void getLocation() async {
+    try {
+      LocationPermission permission = await Geolocator.requestPermission();
+      Position position = await Geolocator.getCurrentPosition(
+          desiredAccuracy: LocationAccuracy.high);
+      print(position);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void fetchData() async {
+    String url = 'https://samples.openweathermap.org/data/2.5/weather?'
+        'q=London&appid=b1b15e88fa797225412429c1c50c122a1';
+    final http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String json = response.body;
+      var myJson = jsonDecode(json)['weather'][0]['description'];
+      var windSpeed = jsonDecode(json)['wind']['speed'];
+      var id = jsonDecode(json)['id'];
+      print(myJson);
+      print(windSpeed);
+      print(id);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              getLocation();
-            },
-            child: const Text('get my location')),
+        child: ElevatedButton(onPressed: null, child: Text('get my location')),
       ),
     );
   }
